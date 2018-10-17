2c7a21b39a4fc1d32b18de7f429e0e756d2878b5
Luis Guitron <legl_1995@hotmail.com>
2018-10-17 10:55:58 -0500

    Training set and Test set

--- neuralnet/main.py
+++ neuralnet/main.py
@@ -1,19 +1,31 @@
 import tensorflow as tf
+import numpy as np
 from logistic_model import logistic_model 
 from read_dataset import *
 
-# Load training dataset    
-X, Y = read_dataset('../data/trainset.csv')
+# Load and shuffle dataset
+dataset = read_dataset('../data/trainset.csv')
+np.random.shuffle(dataset)
+
+
+# Separate 60% Training set / 40% Test Set
+train_test_ratio = 0.6
+trainset_size    = int(np.round(dataset.shape[0] * train_test_ratio))
+testset_size     = dataset.shape[0] - trainset_size
+
+train_dataset = dataset[0:trainset_size,:]
+test_dataset  = dataset[trainset_size:,:]
+train_X, train_Y = separate_labels(train_dataset)
+test_X , test_Y  = separate_labels(test_dataset)
 
 # Network parameters
-input_units   = X.shape[1]
+input_units   = train_X.shape[1]
 hiddent_units = 30
 output_units  = 1
 
 # Learning parameters
-num_iterations = 100
-learning_rate  = 0.5
-
+num_iterations = 1000
+learning_rate  = 0.03
 
 model = logistic_model(input_units, hiddent_units, output_units)
 
@@ -25,10 +37,11 @@ session = tf.Session()
 session.run(tf.global_variables_initializer())
 
 for i in range(num_iterations):
-    session.run(model.grad_descent, feed_dict={model.X: X, model.Y: Y})
+    session.run(model.grad_descent, feed_dict={model.X: train_X, model.Y: train_Y})
 
-#predict       = session.run(model.predict, feed_dict={model.X: X})
-#round_predict = session.run(model.rounded_prediction, feed_dict={model.X: X})
-#mistakes      = session.run(model.mistakes, feed_dict={model.X: X, model.Y: Y})
-accuracy       = session.run(model.accuracy, feed_dict={model.X: X, model.Y: Y})
-print("Training accuracy:" , str.format('{0:.3f}', accuracy*100), "%")
+train_accuracy = session.run(model.accuracy, feed_dict={model.X: train_X, model.Y: train_Y})
+test_accuracy  = session.run(model.accuracy, feed_dict={model.X: test_X, model.Y: test_Y})
+
+test_predict = session.run(model.predict, feed_dict={model.X: test_X, model.Y: test_Y})
+print("Training Set accuracy:" , str.format('{0:.3f}', train_accuracy*100), "%")
+print("Test Set accuracy:" , str.format('{0:.3f}', test_accuracy*100), "%")
--- neuralnet/read_dataset.py
+++ neuralnet/read_dataset.py
@@ -4,6 +4,39 @@ def read_dataset(path_to_dataset_file):
     """ 
         Returns:
         
+        dataset (numpy.ndarray): matrix with feature vector + label on last column
+                                                
+                                                dataset = [[x01,x02,...,x030,y0],
+                                                           [x11,x12,...,x130,y1],
+                                                           [x11,x12,...,x130,y2],
+                                                           ...
+                                                           ]
+    """
+    dataset  = []
+    
+
+    indexFile = open(path_to_dataset_file, 'r')
+    for sample in indexFile:
+        last_row = [] 
+        
+        # Values separated by commas
+        values = sample.split(",")
+
+        for i in range(len(values)):
+            last_row.append(float(values[i]))
+        
+        # Convert labels to {0,1} format in case they are in {-1,1}
+        if(last_row[len(last_row)-1]==-1):
+            last_row[len(last_row)-1]=0
+        
+        dataset.append(last_row)
+    dataset = np.array(dataset)
+    return dataset
+
+def separate_labels(dataset):
+    """
+        Returns:
+        
         X(numpy.ndarray): sample feature matrix X = [[x1],
                                                      [x2],
                                                      [x3],
@@ -18,33 +51,16 @@ def read_dataset(path_to_dataset_file):
     """
     X = []
     Y = []
-
-    indexFile = open(path_to_dataset_file, 'r')
-    for sample in indexFile:
-        
-        
+    for i in range(dataset.shape[0]):
         last_x = []
         last_y = []
-        currentNumber = ""
-        #last_x.append(1)     # Bias feature
-        
-        # Values separated by commas
-        values = sample.split(",")
-        
-        for i in range(len(values)-1):
-            last_x.append(float(values[i]))
-
-        # Last value is the label
-        last_y.append(float(values[len(values)-1]))
-        
-        # Convert labels to {0,1} format in case they are in {-1,1}
-        if(last_y[0]==-1):
-            last_y[0]=0
-
+        for j in range(dataset.shape[1] - 1):
+            last_x.append(dataset[i][j])
+        last_y.append(dataset[i][dataset.shape[1] - 1])
         X.append(last_x)
         Y.append(last_y)
-    
+
     X = np.array(X)
     Y = np.array(Y)
-
+    
     return X, Y
