74a739a54ba71656fa7be6ada80cc3981341ba09
Luis Guitron <legl_1995@hotmail.com>
2018-10-27 14:46:47 -0500

    Scrapper that receives URL as parameter

--- featureSelector/featureSelector.py
+++ featureSelector/featureSelector.py
@@ -1,5 +1,8 @@
-import urllib.request
+#import urllib.request
 
+
+# Feature 1.3.5
+'''
 def iframeTag(url):
     fp = urllib.request.urlopen(url)
     mybytes = fp.read()
@@ -12,7 +15,15 @@ def iframeTag(url):
         return -1           # Phishing
     return 1                # Legitimate
     
+'''
+
+'''
+def iframeTag(url):
     
+    if "<iframe" in open('../PI/phish_site.html').read():
+        return -1           # Phishing
+    return 1                # Legitimate
     
-print(iframeTag('https://www.w3schools.com/tags/tag_iframe.asp'))
-print(iframeTag('https://www.wikipedia.org/'))
+print(iframeTag("https://www.w3schools.com/html/html_iframe.asp"))
+'''
+#print(iframeTag('https://www.wikipedia.org/'))
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from neuralnet import train_model
 from keywordSelector import textRazor
+import subprocess
+import numpy as np
 
 '''
 Registered sites
 '''
 
 # Train Tensorflow neural network (or load when previously trained)
-model = train_model.train_model('data/trainset.csv', hidden_units = 30, num_iterations = 1000, learning_rate = 0.03)
+#model = train_model.train_model('data/trainset.csv', hidden_units = 30, num_iterations = 1000, learning_rate = 0.03)
 
 
-# Get registered websites from database
+# Get websites selected by the user
 registered_websites = []
-registered_websites.append("https://www2.deloitte.com/mx/es.html")
+registered_websites.append("https://www.paypal.com/mx")
 
 # Loop for each registered website
 for r_website in registered_websites:
@@ -23,18 +25,31 @@ for r_website in registered_websites:
     # Get keywords for each website (textRazor API)
     keywords = textRazor.getKeywordsArray(r_website, 0.5, 0.5)
 
+
+    # Upload registered website info in database
+    
     '''
     Phishing sites
     '''
-    # Get array of possible phishing websites for this registered website
+    # Get array of possible phishing websites for this registered website (Google Search)
+    phishing_websites = []
+    #phishing_websites.append("https://www.wikipedia.org")
+    phishing_websites.append("https://www.w3schools.com/html/html_iframe.asp")
+
+    
+    for p_website in phishing_websites:
+        print("Phishing Website: ", p_website)
+
+        # Run Scrapper for current phishing website
+        p = subprocess.call(["scrapy", "crawl", "phishingSpider", "-a", "domain=" + p_website], cwd="PI")
+        #p.wait()
+        
+        
+        # Get feature vector (30 features) for current phishing website
+        
 
 
-    # Get feature vector (30 features) for each possible phishing website
+        # Make prediction for phishing probability for current site
 
 
-
-
-    # Get phishing probabilities for possible phishing sites
-
-
-    # Upload info to database
+    # Upload Phishing website probabilities
