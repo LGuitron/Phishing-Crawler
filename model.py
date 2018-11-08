9f0cc6aa2855fb8b35fc5583f923ee95e3ec9d15
Daniel Strausz <d.strausz@me.com>
2018-11-08 17:20:11 -0600

    Flask and firebase integration

--- model.py
+++ model.py
@@ -5,14 +5,33 @@ from featureSelector import featureSelector
 from featureSelector import featureDict
 import subprocess
 import numpy as np
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+import pyrebase
+
+app = Flask(__name__)
+CORS(app)
+cors = CORS(app, resources={r"/api/": {"origins": "*"}})
+app.config['CORS_HEADERS'] = '*'
+
+config = {
+    "apiKey": "AIzaSyAVXnv4lUTrQc7mBJai4eFTxGLYoDZzVUw",
+    "authDomain": "deloitte-scrapper.firebaseapp.com",
+    "databaseURL": "https://deloitte-scrapper.firebaseio.com",
+    "storageBucket": "deloitte-scrapper.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 
+@app.route('run/<string:company_name>/<string:domain_2_check>')
 def run(company_name, domain_2_check):
 
-    #Possible negative features
+    # Possible negative features
     # 14, 15
-    # Array of implemented features (numbers from 1 - 30 in order) 
-    implemented_features = [6,8,12,23]
+    # Array of implemented features (numbers from 1 - 30 in order)
+    implemented_features = [6, 8, 12, 23]
 
     # Train Tensorflow neural network (or load when previously trained)
     model = train_model.train_model('data/trainset.csv', implemented_features , hidden_units = 30, num_iterations = 1000, learning_rate = 0.03)
@@ -30,15 +49,15 @@ def run(company_name, domain_2_check):
 
     # Loop for each registered website
     for r_website in registered_websites:
-        print("\nLegitimate Website: " , r_website, "\n")
-        
+        print("\nLegitimate Website: ", r_website, "\n")
+
         keywords = googleSearch.getKeywordsArray(r_website, 0.5, 0.5)
         json = googleSearch.googleSearch(keywords, company_name)
 
         for entry in json:
             if(json[entry]['searchInformation']['totalResults'] != '0'):
                 print("------------------------")
-                print("Keyword: " , entry)
+                print("Keyword: ", entry)
                 print("------------------------")
                 for item in json[entry]['items']:
 
@@ -48,7 +67,7 @@ def run(company_name, domain_2_check):
                     p_domain = p_website.split("//")[-1].split("/")[0]
                     if p_domain not in trusted_domains:
 
-                        print("Phishing Website: " , p_website, "\n")
+                        print("Phishing Website: ", p_website, "\n")
 
                         # Run Scrapper for current phishing website
                         p = subprocess.call(["scrapy", "crawl", "phishingSpider", "-a", "domain=" + p_website], cwd="PI")
@@ -58,23 +77,25 @@ def run(company_name, domain_2_check):
                         for i in range(len(implemented_features)):
                             feature_vector[0,i] = featureDict.dictionary[implemented_features[i]](p_website)
 
-                        
                         print("Feature Vector: ")
                         print(feature_vector)
 
                         # Make prediction for phishing probability for current site
                         legit_probability = model.session.run(model.predict, feed_dict={model.X: feature_vector})
-                        print("Phishing probability: " , str.format('{0:.3f}', (1-legit_probability[0,0])*100), "%", "\n")
+                        print("Phishing probability: ", str.format('{0:.3f}', (1 - legit_probability[0, 0]) * 100), "%", "\n")
 
                         # Add domain to trusted domains if probability is below threshold
-                        if (1-legit_probability[0,0] < trust_threshold):
+                        if (1 - legit_probability[0, 0] < trust_threshold):
                             trusted_domains.append(p_domain)
-
     # Write list of trusted_domains in file at end of execution
     with open('trusted_domains.txt', 'w') as f:
         for item in trusted_domains:
+            db.child(company_name).child("trusted").update(item)
             f.write("%s\n" % item)
 
-company_name   = "Paypal"
-domain_2_check = "https://www.paypal.com"
-run(company_name, domain_2_check)
+#company_name   = "Paypal"
+#domain_2_check = "https://www.paypal.com"
+#run(company_name, domain_2_check)
+
+if __name__ == '__main__':
+    app.run()
