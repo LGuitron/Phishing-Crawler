120023d01860de70e15fbabb945c6e2d75846951
Daniel Strausz <d.strausz@me.com>
2018-11-12 17:22:10 -0600

    Finished firebase implementation

--- model.py
+++ model.py
@@ -24,8 +24,12 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
+trusted_domains = []
+suspicious_domains = []
+phishing_domains = []
 
-@app.route('run/<string:company_name>/<string:domain_2_check>')
+
+# @app.route('run/<string:company_name>/<string:domain_2_check>')
 def run(company_name, domain_2_check):
 
     # Possible negative features
@@ -41,10 +45,15 @@ def run(company_name, domain_2_check):
     registered_websites.append(domain_2_check)
 
     # Get list of trusted domains to avoid checking them with the tensorflow model
-    with open('trusted_domains.txt') as f:
-        trusted_domains = f.readlines()
-        trusted_domains = [x.strip() for x in trusted_domains] 
-    trust_threshold = 0.01                                                # Trust websites if phishing probability is below this value
+    # with open('trusted_domains.txt') as f:
+    #     trusted_domains = f.readlines()
+    #     trusted_domains = [x.strip() for x in trusted_domains] 
+    # trust_threshold = 0.01   
+
+    domains_fb = db.child(company_name).child("trusted").get()
+    domains_fb = domains_fb.val()
+    for key, value in domains_fb.items():
+        trusted_domains.append(key.replace("_", "."))   # Trust websites if phishing probability is below this value
 
 
     # Loop for each registered website
@@ -87,15 +96,27 @@ def run(company_name, domain_2_check):
                         # Add domain to trusted domains if probability is below threshold
                         if (1 - legit_probability[0, 0] < trust_threshold):
                             trusted_domains.append(p_domain)
+                        elif (1 - legit_probability[0, 0] < 0.3):
+                            suspicious_domains.append(p_domain)
+                        else:
+                            phishing_domains.append(p_domain)
     # Write list of trusted_domains in file at end of execution
-    with open('trusted_domains.txt', 'w') as f:
-        for item in trusted_domains:
-            db.child(company_name).child("trusted").update(item)
-            f.write("%s\n" % item)
+    for item in trusted_domains:
+        item = item.replace(".", "_")
+        db.child(company_name).child("trusted").update({item: 1})
+
+    for item in phishing_domains:
+        item = item.replace(".", "_")
+        db.child(company_name).child("phishing").update({item: 1})
+
+    for item in suspicious_domains:
+        item = item.replace(".", "_")
+        db.child(company_name).child("suspicious").update({item: 1})
 
 #company_name   = "Paypal"
 #domain_2_check = "https://www.paypal.com"
 #run(company_name, domain_2_check)
 
-if __name__ == '__main__':
-    app.run()
+run("Deloitte", "https://www2.deloitte.com/mx/es.html")
+# if __name__ == '__main__':
+#     app.run()
