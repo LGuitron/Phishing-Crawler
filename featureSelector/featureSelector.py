029fecb21ec470c0753ea696fe8e775d3c2fbbae
Luis Guitron <legl_1995@hotmail.com>
2018-11-14 10:43:26 -0600

    Fixed features 8, 14 & 15 and added test function

--- featureSelector/featureSelector.py
+++ featureSelector/featureSelector.py
@@ -22,20 +22,21 @@ def pref_suf(url):
 def ssl_state(url):
     
     # TODO do this wih openSSL library
-    return https_token(url)
-    '''
+    #return https_token(url)
     protocol = url.split("//")[0]
     if "https" not in protocol:
         return -1            # Phishing
     
     domain = url.split("//")[-1].split("/")[0]
-    cert=ssl.get_server_certificate((domain, 443))
-    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
-    if(x509.has_expired()):
-        return -1            # Phishing
+    try:
+        cert=ssl.get_server_certificate((domain, 443))
+        x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+        if(x509.has_expired()):
+            return -1            # Phishing
+        return 1                 # Legitimate
     
-    return 1                 # Legitimate
-    '''
+    except:
+        return 0
 
 # Feature #12 (1.1.12)
 def https_token(url):
@@ -47,55 +48,84 @@ def https_token(url):
 
 # Feature #14 (1.2.2)
 def url_of_anchor(url):
+    
+    # Return 1 when pdf file
+    split_url = url.split(".")
+    
+    if split_url[len(split_url)-1] == "pdf":
+        return 1
+
     html_file = "PI/phish-site.html"
     
-    with open(html_file) as myfile:
-        html = myfile.read().replace('\n', '')
-    
-    soup = BeautifulSoup(html, "html.parser")
-    
-    links = []
-    for tag in soup.find_all("a"):
-        link = str(tag).split("href=\"")[-1].split("\"")[0]
-        links.append(link)
-    
-    link_own = link_ownership(url,links)
-    if link_own > 0.69:
-        return 1            # Legitimate
-    elif link_own > 0.33:
-        return 0            # Suspicious
-    else:
-        return -1           # Phishing
+    try:
+        with open(html_file) as myfile:
+            html = myfile.read().replace('\n', '')
+        soup = BeautifulSoup(html, "html.parser")
+        
+        links = []
+        for tag in soup.find_all("a"):
+            link = str(tag).split("href=\"")[-1].split("\"")[0]
+            links.append(link)
+        
+        link_own = link_ownership(url,links)
+        if link_own > 0.69:
+            return 1            # Legitimate
+        elif link_own > 0.33:
+            return 0            # Suspicious
+        else:
+            return -1           # Phishing
+    except:
+        return 0
 
 # Feature #15 (1.2.3)
 def tag_links(url):
+    
+    # Return 1 when pdf file
+    split_url = url.split(".")
+    
+    if split_url[len(split_url)-1] == "pdf":
+        return 1
+    
     html_file = "PI/phish-site.html"
     
-    with open(html_file) as myfile:
-        html = myfile.read().replace('\n', '')
-    
-    soup = BeautifulSoup(html, "html.parser")
-    
-    links = []
-    for tag in soup.find_all(["meta","script","link"]):
-        link = str(tag).split("href=\"")[-1].split("\"")[0]
-        links.append(link)
-    
-    link_own = link_ownership(url,links)
-    if link_own > 0.69:
-        return 1            # Legitimate
-    elif link_own > 0.33:
-        return 0            # Suspicious
-    else:
-        return -1           # Phishing
+    try:
+        with open(html_file) as myfile:
+            html = myfile.read().replace('\n', '')
+        
+        soup = BeautifulSoup(html, "html.parser")
+        
+        links = []
+        for tag in soup.find_all(["meta","script","link"]):
+            link = str(tag).split("href=\"")[-1].split("\"")[0]
+            links.append(link)
+        
+        link_own = link_ownership(url,links)
+        if link_own > 0.69:
+            return 1            # Legitimate
+        elif link_own > 0.33:
+            return 0            # Suspicious
+        else:
+            return -1           # Phishing
+    except:
+        return 0
 
 
 # Feature #23 (1.3.5)
 def iframe(url):
-    html_file = "PI/phish-site.html"
-    if "<iframe" in open(html_file).read():
-        return -1           # Phishing
-    return 1                # Legitimate
+
+    # Return 1 when pdf file
+    split_url = url.split(".")
+    
+    if split_url[len(split_url)-1] == "pdf":
+        return 1
+    
+    try:
+        html_file = "PI/phish-site.html"
+        if "<iframe" in open(html_file).read():
+            return -1           # Phishing
+        return 1                # Legitimate
+    except:
+        return 0
 
 
 # Feature #29 (1.4.6)
@@ -117,6 +147,8 @@ def link_ownership(url, links):
             if(url_domain==link_domain):
                 owned += 1
 
+    if(len(links)==0):
+        return 1
     return owned/len(links)
 
 # Feature # 24 (1.4.1)
--- model.py
+++ model.py
@@ -26,7 +26,9 @@ db = firebase.database()
 
 trusted_domains = []
 suspicious_domains = []
+suspicious_probabilities = []
 phishing_domains = []
+phishing_probabilities = []
 
 def run(company_name, domain_2_check):
 
@@ -48,8 +50,11 @@ def run(company_name, domain_2_check):
 
     domains_fb = db.child(company_name).child("trusted").get()
     domains_fb = domains_fb.val()
-    for key, value in domains_fb.items():
-        trusted_domains.append(key.replace("_", "."))   # Trust websites if phishing probability is below this value
+    
+    # Add trusted domains if entry is already specified in DB
+    if domains_fb != None:
+        for key, value in domains_fb.items():
+            trusted_domains.append(key.replace("_", "."))   # Trust websites if phishing probability is below this value
 
     # Loop for each registered website
     for r_website in registered_websites:
@@ -66,7 +71,7 @@ def run(company_name, domain_2_check):
                 for item in json[entry]['items']:
 
                     p_website = item['link']
-
+                    
                     # Check if this link is in the set of trusted domains
                     p_domain = p_website.split("//")[-1].split("/")[0]
                     if p_domain not in trusted_domains:
@@ -94,22 +99,64 @@ def run(company_name, domain_2_check):
                             trusted_domains.append(p_domain)
                         elif (1 - legit_probability[0, 0] < 0.3):
                             suspicious_domains.append(p_domain)
+                            suspicious_probabilities.append(1 - legit_probability[0, 0])
                         else:
                             phishing_domains.append(p_domain)
+                            phishing_probabilities.append(1 - legit_probability[0, 0])
 
     # Write list of trusted_domains in file at end of execution
     for item in trusted_domains:
         item = item.replace(".", "_")
-        db.child(company_name).child("trusted").update({item: 1})
+        db.child(company_name).child("trusted").update({item: 0})
 
-    for item in phishing_domains:
+    for i in range(len(suspicious_domains)):
+        item = suspicious_domains[i]
+        prob = suspicious_probabilities[i]
         item = item.replace(".", "_")
-        db.child(company_name).child("phishing").update({item: 1})
+        db.child(company_name).child("suspicious").update({item: prob})
 
-    for item in suspicious_domains:
+    #for item in suspicious_domains:
+    for i in range(len(phishing_domains)):
+        item = phishing_domains[i]
+        prob = phishing_probabilities[i]
         item = item.replace(".", "_")
-        db.child(company_name).child("suspicious").update({item: 1})
+        db.child(company_name).child("suspicious").update({item: prob})
 
+
+# Helper function to get phishing probability of a given website
+def getPhishingProbability(test_domains, implemented_features):
+    
+    model = train_model.train_model('data/trainset.csv', implemented_features , hidden_units = 30, num_iterations = 1000, learning_rate = 0.03)
+
+    for p_website in test_domains:
+        # Check if this link is in the set of trusted domains
+        p_domain = p_website.split("//")[-1].split("/")[0]
+        print("Phishing Website: ", p_website, "\n")
+
+        # Run Scrapper for current phishing website
+        p = subprocess.call(["scrapy", "crawl", "phishingSpider", "-a", "domain=" + p_website], cwd="PI")
+
+        # Get feature vector (30 features) for current phishing website
+        feature_vector = np.zeros((1, len(implemented_features)))
+        for i in range(len(implemented_features)):
+            feature_vector[0,i] = featureDict.dictionary[implemented_features[i]](p_website)
+
+        print("Feature Vector: ")
+        print(feature_vector)
+
+        # Make prediction for phishing probability for current site
+        legit_probability = model.session.run(model.predict, feed_dict={model.X: feature_vector})
+        print("Phishing probability: ", str.format('{0:.3f}', (1 - legit_probability[0, 0]) * 100), "%", "\n")
+
+
+# Get probability of a particular phishing website
+#implemented_features = [2,6,8,12,23,24]
+#test_domains = ["http://idunsfcs.com/ssv/cascome.htm"]
+#getPhishingProbability(test_domains, implemented_features)
+
+
+# Look for phishing website based on company name an domain
+run("Paypal", "https://www.paypal.com/mx/home")
+run("Facebook", "https://www.facebook.com/")
 run("Deloitte", "https://www2.deloitte.com/mx/es.html")
-# if __name__ == '__main__':
-#     app.run()
+run("Microsoft", "https://onedrive.live.com/about/es-mx/")
